8dfec05bbbb4b0ddb3ea97b2bd71e68aa4d891d9
BADHRIRAJAN T <badhrirajan2211@gmail.com>
2023-08-09 10:39:38 +0530

    Backend

--- Controllers/usercontroller.js
+++ Controllers/usercontroller.js
@@ -93,7 +93,7 @@ UserRouter.post('/forgot-password', (req,res) => {
       from: 'badhrirajan2211@gmail.com',
       to: email,
       subject: 'Reset your Password',
-      text: `https://warm-dragon-c11fcf.netlify.app/reset/${user._id}/${token}`
+      text: `http://localhost:3000/reset/${user._id}/${token}`
     };
     
     transporter.sendMail(mailOptions, function(error, info){
